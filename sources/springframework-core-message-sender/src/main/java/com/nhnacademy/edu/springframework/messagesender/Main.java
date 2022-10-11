0faa879baf1f463dcf7243b86c1bc32778298ff8
alanhakhyeonsong <songs4805@naver.com>
2022-10-11 13:17:05 +0900

    Refactor: IoC Container 기반으로 변경

--- sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -1,9 +1,15 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
-        new MessageSendService(new EmailMessageSender()).doSendMessage();
-        new MessageSendService(new SmsMessageSender()).doSendMessage();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        MessageSender emailMessageSender = context.getBean("emailMessageSender", EmailMessageSender.class);
+        MessageSender smsMessageSender = context.getBean("smsMessageSender", SmsMessageSender.class);
+
+        emailMessageSender.sendMessage(new User("songs4805@naver.com", "82-10-1111-1111"), "message");
+        smsMessageSender.sendMessage(new User("songs4805@naver.com", "82-10-1111-1111"), "message");
     }
 }
