f72b64fdb8f96e66e33499140fd431e4baef1fa3
alanhakhyeonsong <songs4805@naver.com>
2022-10-11 15:27:19 +0900

    Feat: DI Constructor 실습

--- sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -6,13 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        User user = new User("songs4805@naver.com", "82-10-1111-1111");
 
-        context.getBean("emailMessageSender", EmailMessageSender.class).sendMessage(user, "message");
-        context.getBean("smsMessageSender", SmsMessageSender.class).sendMessage(user, "message");
-
-        context.getBean("emailMessageSender", EmailMessageSender.class).sendMessage(user, "message");
-        context.getBean("smsMessageSender", SmsMessageSender.class).sendMessage(user, "message");
+        context.getBean("messageSendService", MessageSendService.class).doSendMessage();
 
         context.close();
     }
