a3e1b29b4d4197ad68affe0a343ac1b03e3e59ba
alanhakhyeonsong <songs4805@naver.com>
2022-10-11 16:16:31 +0900

    Feat: DI Setter Injection 실습

--- sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
+++ sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
@@ -8,6 +8,14 @@ public class MessageSendService {
         this.messageSender = messageSender;
     }
 
+    public MessageSendService() {
+    }
+
+    public void setMessageSender(MessageSender messageSender) {
+        System.out.println("created by setMessageSender");
+        this.messageSender = messageSender;
+    }
+
     public void doSendMessage() {
         messageSender.sendMessage(new User("songs4805@naver.com", "82-10-1111-1111"), "message");
     }
