deef3701a777e77954b76aea0f5548a1c0dd2915
alanhakhyeonsong <songs4805@naver.com>
2022-10-12 10:19:18 +0900

    Refactor: Custom @Qualifier 설정

--- sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
+++ sources/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
@@ -1,14 +1,13 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public class MessageSendService {
 
     private final MessageSender messageSender;
 
     @Autowired
-    public MessageSendService(@Qualifier(value = "smsMessageSender") MessageSender messageSender) {
+    public MessageSendService(@Sms MessageSender messageSender) {
         this.messageSender = messageSender;
     }
 
